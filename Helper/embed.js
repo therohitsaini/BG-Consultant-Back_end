13358f6551746a1297fa1d4462bd9eba477d8ed2
Rohit suman <rohit.sangod74@gmail.com>
2026-03-13 11:05:02 +0530

    add controller auto setup store front

--- Helper/embed.js
+++ Helper/embed.js
@@ -1,23 +1,28 @@
 (function () {
-  function loadReactApp() {
-    console.log("Embed running");
+  console.log("STEP 1 - embed start");
+
+  function init() {
+    console.log("STEP 2 - init running");
 
     const root = document.createElement("div");
     root.id = "root";
 
     document.body.appendChild(root);
 
+    console.log("STEP 3 - root added");
+
     const script = document.createElement("script");
     script.src =
       "https://lan-hay-king-bali.trycloudflare.com/static/js/main.918d4656.js";
-    script.defer = true;
 
     document.body.appendChild(script);
+
+    console.log("STEP 4 - react script added");
   }
 
   if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", loadReactApp);
+    document.addEventListener("DOMContentLoaded", init);
   } else {
-    loadReactApp();
+    init();
   }
 })();
