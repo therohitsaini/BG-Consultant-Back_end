4886d36b53d0db9f40122f63f572ca36fecc6a13
Rohit suman <rohit.sangod74@gmail.com>
2026-03-13 15:19:58 +0530

    add controller auto setup store front

--- Helper/embed.js
+++ Helper/embed.js
@@ -17,7 +17,7 @@
 
     const script = document.createElement("script");
     script.src =
-      "https://lan-hay-king-bali.trycloudflare.com/static/js/main.918d4656.js";
+      "https://lan-hay-king-bali.trycloudflare.com/static/js/main.73191a4f.js";
 
     script.async = true;
     document.body.appendChild(script);
