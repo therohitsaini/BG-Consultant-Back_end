0840dbf1421343204aed8bf8b18a5481aa80e7c8
Rohit suman <rohit.sangod74@gmail.com>
2026-03-13 12:31:05 +0530

    add controller auto setup store front

--- index.js
+++ index.js
@@ -63,23 +63,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get("/embed.js", (req, res) => {
-//   res.setHeader("Content-Type", "application/javascript");
-
-//   res.send(`
-//     console.log("BigCommerce embed script loaded");
-//     const div = document.createElement("div");
-//     div.innerHTML = "React Widget Loaded";
-//     div.style.position = "fixed";
-//     div.style.bottom = "20px";
-//     div.style.right = "20px";
-//     div.style.background = "black";
-//     div.style.color = "white";
-//     div.style.padding = "10px";
-//     document.body.appendChild(div);
-//   `);
-// });
-
+app.get("/embed.js", (req, res) => {
+  res.setHeader("Content-Type", "application/javascript");
+  res.sendFile(path.join(__dirname, "Helper/embed.js"));
+});
 const { callRoutes } = require("./Routes/videoCallRotes");
 const { signinSignupRouter } = require("./Routes/signin-signupRoute");
 const { userDetailsRouter } = require("./Routes/userDetailsRoutes");
