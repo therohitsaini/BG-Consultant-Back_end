c0147530bf2bb781cac7732df77aab1a55d58aa3
Rohit suman <rohit.sangod74@gmail.com>
2026-03-13 17:28:07 +0530

    add controller auto setup store front

--- Helper/embed.js
+++ Helper/embed.js
@@ -17,28 +17,35 @@
 
 //   document.body.appendChild(script);
 // })();
+
 (function () {
-  console.log("Embed start");
 
-  let container = document.getElementById("consultant-root");
-
-  if (!container) {
-    container = document.createElement("div");
-    container.id = "consultant-root";
-    document.body.appendChild(container);
-  }
-  console.log("container", container);
-  const script = document.createElement("script");
-  script.src = "https://lan-hay-king-bali.trycloudflare.com/static/js/main.523b49a3.js";
-  console.log("script", script);
-  script.onload = function () {
-    console.log("React script loaded");
-
-    if (window.mountConsultantApp) {
-      console.log("window.mountConsultantApp", window.mountConsultantApp);
-      window.mountConsultantApp("consultant-root");
+    console.log("Embed start");
+  
+    let root = document.getElementById("root");
+  
+    if (!root) {
+      root = document.createElement("div");
+      root.id = "root";
+      document.body.appendChild(root);
     }
-  };
-
-  document.body.appendChild(script);
-})();
+  
+    const script = document.createElement("script");
+  
+    script.src =
+    "https://lan-hay-king-bali.trycloudflare.com/static/js/main.73191a4f.js";
+  
+    script.type = "text/javascript";
+    script.crossOrigin = "anonymous";
+  
+    script.onload = function () {
+      console.log("React bundle loaded");
+    };
+  
+    script.onerror = function () {
+      console.log("Script load failed");
+    };
+  
+    document.body.appendChild(script);
+  
+  })();
