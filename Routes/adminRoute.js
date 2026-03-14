0d1cce8bc06841d3078f78c159ef69addee73829
Rohit suman <rohit.sangod74@gmail.com>
2026-03-14 17:42:09 +0530

    add debugs

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -7,18 +7,18 @@ const adminRoute = express.Router();
 
 adminRoute.get("/admin/:adminId", adminController);
 adminRoute.post("/admin/voucher/:adminId", voucherController);
-adminRoute.get("/activity/transactions/:adminId", verifyShopifyToken, getTransactionController);
-adminRoute.get("/user/consultant/:adminId", verifyShopifyToken, getUserConsultantController);
-adminRoute.get("/shop/all-user/:adminId", verifyShopifyToken, getShopAllUserController);
-adminRoute.get("/shop/all-consultant/:adminId", verifyShopifyToken, getShopAllConsultantController);
-adminRoute.post("/update-wallet/:adminId", verifyShopifyToken, updateUserConsultantController);
-adminRoute.post("/app-enable-and-disable/:adminId", verifyShopifyToken, appEnableAndDisableController);
-adminRoute.get("/shop/billing-status/:adminId", verifyShopifyToken, checkAppBillingController)
-adminRoute.delete("/delete/voucher/:shopId/:voucherId", verifyShopifyToken, voucherHandlerController)
-adminRoute.put("/admin/voucher-updates/:shopId/:voucherId", verifyShopifyToken, updatesVoucherController)
-adminRoute.get("/withdrawal-requests/:adminId", verifyShopifyToken, getWithdrawalRequest)
-adminRoute.put("/update/widthrwal/req/:adminId", verifyShopifyToken, updateConsultantWidthrawalRequest)
-adminRoute.put("/declin/widthrwal/req/:transactionId", verifyShopifyToken, declineWithdrawalRequest)
-adminRoute.put("/admin/update-percentage/:adminId", verifyShopifyToken, updateAdminPercentage)
+adminRoute.get("/activity/transactions/:adminId" ,getTransactionController);
+adminRoute.get("/user/consultant/:adminId", getUserConsultantController);
+adminRoute.get("/shop/all-user/:adminId", getShopAllUserController);
+adminRoute.get("/shop/all-consultant/:adminId", getShopAllConsultantController);
+adminRoute.post("/update-wallet/:adminId", updateUserConsultantController);
+adminRoute.post("/app-enable-and-disable/:adminId", appEnableAndDisableController);
+adminRoute.get("/shop/billing-status/:adminId", checkAppBillingController)
+adminRoute.delete("/delete/voucher/:shopId/:voucherId", voucherHandlerController)
+adminRoute.put("/admin/voucher-updates/:shopId/:voucherId", updatesVoucherController)
+adminRoute.get("/withdrawal-requests/:adminId", getWithdrawalRequest)
+adminRoute.put("/update/widthrwal/req/:adminId", updateConsultantWidthrawalRequest)
+adminRoute.put("/declin/widthrwal/req/:transactionId", declineWithdrawalRequest)
+adminRoute.put("/admin/update-percentage/:adminId", updateAdminPercentage)
 
 module.exports = { adminRoute };    
