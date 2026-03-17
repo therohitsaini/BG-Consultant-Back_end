75ecfbcaf1844a6a7eafd8f776c815c116b7aa31
Rohit suman <rohit.sangod74@gmail.com>
2026-03-17 15:48:50 +0530

    update domain

--- Helper/embed.js
+++ Helper/embed.js
@@ -1,16 +1,19 @@
 (function () {
   const root = document.getElementById("consultant-root");
   if (!root) return;
-
+console.log("React Bundle Loading..."); 
   const link = document.createElement("link");
   link.rel = "stylesheet";
+  console.log("CSS Loading...");
   link.href =
     "https://helping-highs-entered-discovery.trycloudflare.com/static/css/main.css";
   document.head.appendChild(link);
 
   const script = document.createElement("script");
+  console.log("JS Loading...");
   script.src =
     "https://helping-highs-entered-discovery.trycloudflare.com/static/js/main.96fd388c.js";
   script.async = true;
   document.body.appendChild(script);
+  console.log("JS Loaded...");
 })();
