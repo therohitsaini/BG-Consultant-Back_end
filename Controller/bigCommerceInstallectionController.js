6b0df1e54355d5b897897c5e78e629b1868c3d35
Rohit suman <rohit.sangod74@gmail.com>
2026-03-19 10:51:54 +0530

    domain update

--- Controller/bigCommerceInstallectionController.js
+++ Controller/bigCommerceInstallectionController.js
@@ -61,7 +61,7 @@ const installBigCommerce = async (req, res) => {
         body: `
        <div style="width: 100%;">
         <iframe 
-          src="https://expert-occupational-definitely-sink.trycloudflare.com?storeHash=${storeHash}&userId=${userId}" 
+          src="https://mileage-west-picking-became.trycloudflare.com?storeHash=${storeHash}&userId=${userId}" 
           id="consultant-iframe"
           style="width: 100%; border: none; overflow: hidden;" 
           scrolling="no"
