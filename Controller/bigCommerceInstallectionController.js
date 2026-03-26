08ca1d64887dde9e02d4c747575187ecc1be077c
Rohit suman <rohit.sangod74@gmail.com>
2026-03-26 15:13:21 +0530

    domain update

--- Controller/bigCommerceInstallectionController.js
+++ Controller/bigCommerceInstallectionController.js
@@ -70,7 +70,7 @@ const installBigCommerce = async (req, res) => {
         body: `
        <div style="width: 100%;">
         <iframe 
-          src="https://operations-brands-concentrate-fee.trycloudflare.com?storeHash=${storeHash}&userId=${userId}" 
+          src="https://carlos-voip-turner-mechanism.trycloudflare.com?storeHash=${storeHash}&userId=${userId}" 
           id="consultant-iframe"
           style="width: 100%; border: none; overflow: hidden;" 
           scrolling="no"
