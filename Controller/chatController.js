93cf6d4cfb0b41b495e3c9bd6571b972ba4010d5
Rohit suman <rohit.sangod74@gmail.com>
2026-03-31 13:22:41 +0530

    domain update

--- Controller/chatController.js
+++ Controller/chatController.js
@@ -3,60 +3,70 @@ const { MessageModal } = require("../Modal/messageSchema");
 const { ChatList } = require("../Modal/chatListSchema");
 
 const getChatHistory = async (request, response) => {
-    try {
-        const { shopId, consultantId, userId } = request.params;
+  try {
+    const { shopId, consultantId, userId } = request.params;
 
-
-        if (!mongoose.Types.ObjectId.isValid(shopId) ||
-            !mongoose.Types.ObjectId.isValid(consultantId) ||
-            !mongoose.Types.ObjectId.isValid(userId)) {
-            return response.status(400).json({ message: "Invalid IDs" });
-        }
-
-        const chatHistory = await MessageModal.find({
-            shop_id: shopId,
-            $or: [
-                { senderId: userId, receiverId: consultantId },        // user → consultant
-                { senderId: consultantId, receiverId: userId }         // consultant → user
-            ]
-        }).sort({ timestamp: 1 });  // sorting by time
-
-        if (!chatHistory) {
-            return response.status(400).json({ message: "Chat history not found" });
-        }
-
-        return response.status(200).json({
-            success: true,
-            message: "Chat history fetched successfully",
-            chatHistory
-        });
-
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({ message: "Server error" });
+    if (
+      !mongoose.Types.ObjectId.isValid(shopId) ||
+      !mongoose.Types.ObjectId.isValid(consultantId) ||
+      !mongoose.Types.ObjectId.isValid(userId)
+    ) {
+      return response.status(400).json({ message: "Invalid IDs" });
     }
+
+    const chatHistory = await MessageModal.find({
+      shop_id: shopId,
+      $or: [
+        { senderId: userId, receiverId: consultantId }, // user → consultant
+        { senderId: consultantId, receiverId: userId }, // consultant → user
+      ],
+    }).sort({ timestamp: 1 }); // sorting by time
+
+    if (!chatHistory) {
+      return response.status(400).json({ message: "Chat history not found" });
+    }
+
+    return response.status(200).json({
+      success: true,
+      message: "Chat history fetched successfully",
+      chatHistory,
+    });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ message: "Server error" });
+  }
 };
 
-
 const getUserInRecentChat = async (request, response) => {
-    try {
-        const { shopId, userId, consultantId } = request.params;
-        if (!mongoose.Types.ObjectId.isValid(shopId) ||
-            !mongoose.Types.ObjectId.isValid(userId) ||
-            !mongoose.Types.ObjectId.isValid(consultantId)) {
-            return response.status(400).json({ message: "Invalid IDs" });
-        }
-        const userInRecentChat = await ChatList.findOne({ shop_id: shopId, senderId: userId, receiverId: consultantId });
-        if (!userInRecentChat) {
-            return response.status(400).json({ message: "User not found in recent chat" });
-        }
-        userInRecentChat.isRequest = true;
-        await userInRecentChat.save();
-        return response.status(200).json({ success: true, message: "User found in recent chat", });
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({ message: "Server error" });
+  try {
+    const { shopId, userId, consultantId } = request.params;
+    console.log("shopId", shopId);
+    console.log("userId", userId);
+    console.log("consultantId", consultantId);
+    if (
+      !mongoose.Types.ObjectId.isValid(shopId) ||
+      !mongoose.Types.ObjectId.isValid(userId) ||
+      !mongoose.Types.ObjectId.isValid(consultantId)
+    ) {
+      return response.status(400).json({ message: "Invalid IDs" });
     }
+    const userInRecentChat = await ChatList.findOne({
+      shop_id: shopId,
+      senderId: userId,
+      receiverId: consultantId,
+    });
+    if (!userInRecentChat) {
+      return response
+        .status(400)
+        .json({ message: "User not found in recent chat" });
+    }
+    userInRecentChat.isRequest = true;
+    await userInRecentChat.save();
+    return response.status(200).json({ success: true, message: "User found in recent chat" });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ message: "Server error" });
+  }
 };
 
 module.exports = { getChatHistory, getUserInRecentChat };
